051533d54e10f1f4515a6e7cfde3fd68147eabad
Mitch Stonehocker <mtichstonehocker@gmail.com>
2019-02-27 17:09:07 -0500

    useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function App () {
+  const [userQuery, setUserQuery] = useState('')
+
+  const updateUserQuery = event => {
+    setUserQuery(event.target.value)
+  }
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, '_blank')
+  }
+
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      searchQuery()
+    }
+  }
+
   return (
     <div className='App'>
       <h1>Hi Mitch</h1>
+      <div className='form'>
+        <input
+          value={userQuery}
+          onChange={updateUserQuery}
+          onKeyPress={handleKeyPress}
+        />
+        <button onClick={searchQuery}>Serach</button>
+      </div>
     </div>
   )
 }
